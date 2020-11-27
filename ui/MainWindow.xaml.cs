7a05f7ec5061f6986cb1ac177d54be0a545782d9
张睿 <43677361+ZRE-ray@users.noreply.github.com>
2020-11-27 20:12:43 +0800

    1

---
+++ ui/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Database;
+
+namespace 消费中心
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int n;
+            if (textbox1.Text.Trim().Length == 0 || textbox2.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("账号或密码不能为空", "警告",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+            else
+            {
+                string sql = "select* from User_table where Id=" + textbox1.Text + " and Password='" + textbox2.Text + "'";
+                Program p = new Program();
+                p.OpenDB();
+                n = p.Searchlogin(sql);
+                if (n == 0)
+                {
+                    MessageBox.Show("登录失败，用户不存在", "错误",MessageBoxButton.OK,MessageBoxImage.Error);
+                }
+                else
+                {
+                    MessageBox.Show("登录成功", "提示",MessageBoxButton.OK, MessageBoxImage.Information);
+                    Window1 a1 = new Window1();
+                    a1.Show();
+                    this.Visibility = System.Windows.Visibility.Hidden;
+                }
+                p.CloseDB();
+            }
+        }
+
+
+    }
+}
---
+++ ui/Window1.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace 消费中心
+{
+    /// <summary>
+    /// Window1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window2 a2 = new Window2();
+            a2.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 a3 = new Window3();
+            a3.ShowDialog();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Window4 a4 = new Window4();
+            a4.ShowDialog();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Window5 a5 = new Window5();
+            a5.ShowDialog();
+        }
+    }
+}
