0f0b8979127d72e34af2e3adad352fdc058bced4
WeilaiFuture <43932541+WeilaiFuture@users.noreply.github.com>
2020-11-27 21:00:30 +0800

    魏来
    函数实现

--- ui/Window6.xaml.cs
+++ ui/Window6.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO.Ports;
 using System.Threading;
+using WinForm = System.Windows.Forms;
+using System.IO;
 
 namespace 消费中心
 {
@@ -30,16 +32,15 @@ namespace 消费中心
 
         private void RFIDReader_Load(object sender, EventArgs e)
         {
-            if(RFID.IsOpen)
+            Button b = (Button)sender;
+            if (RFID.IsOpen)
             {
-                Button b=(Button)sender;
                 b.Content="打开串口";
                 RFID.Close();
                 
             }
             else
             {
-                Button b=(Button)sender;
                 b.Content="关闭串口";
                 RFID.PortName=comboBox1.Text;
                 RFID.BaudRate=int.Parse(comboBox2.Text);
@@ -50,7 +51,7 @@ namespace 消费中心
                 //RFID.RtsEnable = true;
                 //RFID.Handshake = Handshake.None; 
                 RFID.Open();
-                RFID.ReadTimeout = 200;
+                //RFID.ReadTimeout = 200;
                 // string[] ports = SerialPort.GetPortNames();
                 RFID.DataReceived += new SerialDataReceivedEventHandler(RFID_DataReceivedHandler);
                 // RFID.Close();
@@ -63,7 +64,7 @@ namespace 消费中心
             {
                 for (int i = 0; i < bytes.Length; i++)
                 {
-                    returnStr += bytes[i].ToString("X2");
+                    returnStr += bytes[i].ToString("X2")+" ";
                 }
             }
             return returnStr;
@@ -72,26 +73,49 @@ namespace 消费中心
 
         private void RFID_DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
-            int buffersize = 20;   //十六进制数的大小（假设为6byte）
-            byte[] buffer = new byte[buffersize];   //创建缓冲区
+            string str1="";
             SerialPort sp = (SerialPort)sender;
+            DateTime dt = DateTime.Now;
+            if(Time.IsChecked==true)
+            {
+                str1 += dt.ToString() + " ";
+            }
+            int buffersize = sp.BytesToRead;   //十六进制数的大小（假设为6byte）
+            byte[] buffer = new byte[buffersize];   //创建缓冲区
             sp.Read(buffer, 0, buffersize);
-            string ss;
-            ss = byteToHexStr(buffer); //用到函数byteToHexStr
-            //string indata = sp.ReadExisting();
-            //string indata = sp.ReadExisting();
-            //serialPort1.Read(buffer, 0, buffersize);
-            //string ss = byteToHexStr(indata); //用到函数byteToHexStr
+            if (Show_A.IsChecked == true)
+            {
+                str1 += byteToHexStr(buffer); //用到函数byteToHexStr
+            }
+            else
+            {
+                str1 += System.Text.Encoding.Default.GetString(buffer);
+            }
+            string s = dt.ToString() + " "+ System.Text.Encoding.Default.GetString(buffer);
+            StreamWriter sw = new StreamWriter("log", true, Encoding.GetEncoding("UTF-8"));
+            sw.WriteLine(s);
+            sw.Close();
             Dispatcher.BeginInvoke(new Action(delegate
             {
-
-                RFIDTagInputBox.Text += ss;
-                //RFIDTagInputBox.Text = indata;
+                RFIDTagInputBox.Text +=  str1 ;
             }));
 
         }
         private void SaveFile(object sender, EventArgs e)
         {
+            string path;
+            WinForm.SaveFileDialog dlg = new WinForm.SaveFileDialog();
+            dlg.FileName = "Document";
+            dlg.DefaultExt = ".text";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+            if (dlg.ShowDialog() == WinForm.DialogResult.OK)//若确认为另存为，则状态栏相关数据改变
+            {
+                path = System.IO.Path.GetFullPath(dlg.FileName);
+                StreamWriter sw = new StreamWriter(path, false, Encoding.GetEncoding("UTF-8"));
+                sw.WriteLine(RFIDTagInputBox.Text);
+                sw.Close();
+                MessageBox.Show("保存成功");
+            }
         }
 
     }
