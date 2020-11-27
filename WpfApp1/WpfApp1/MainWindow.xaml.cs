9494fa8496c5942cbbfa86af9b37609fa23b23d5
WeilaiFuture <43932541+WeilaiFuture@users.noreply.github.com>
2020-11-27 18:28:24 +0800

    魏来
    更新打开串口操作

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         private SerialPort RFID=new SerialPort();
-
+      
         public MainWindow()
         {
             InitializeComponent();
@@ -31,25 +31,30 @@ namespace WpfApp1
 
         private void RFIDReader_Load(object sender, EventArgs e)
         {
-            if(RFID.IsOpen())
+            if(RFID.IsOpen)
             {
+                Button b=(Button)sender;
+                b.Content="打开串口";
                 RFID.Close();
+                
             }
             else
             {
-            RFID.PortName=comboBox1.Text;
-            RFID.BaudRate=int.Parse(comboBox2.Text);
-            RFID.DataBits=5+comboBox4.SelectedIndex;
-            RFID.Parity=(Parity)comboBox3.SelectedIndex;
-            RFID.StopBits=(StopBits)(comboBox5.SelectedIndex+1);
-            RFID.Encoding = Encoding.BigEndianUnicode;
-            //RFID.RtsEnable = true;
-            //RFID.Handshake = Handshake.None; 
-            RFID.Open();
-            // RFID.ReadTimeout = 200;
-            // string[] ports = SerialPort.GetPortNames();
-            RFID.DataReceived += new SerialDataReceivedEventHandler(RFID_DataReceivedHandler);
-            // RFID.Close();
+                Button b=(Button)sender;
+                b.Content="关闭串口";
+                RFID.PortName=comboBox1.Text;
+                RFID.BaudRate=int.Parse(comboBox2.Text);
+                RFID.DataBits=5+comboBox4.SelectedIndex;
+                RFID.Parity=(Parity)comboBox3.SelectedIndex;
+                RFID.StopBits=(StopBits)(comboBox5.SelectedIndex+1);
+                RFID.Encoding = Encoding.BigEndianUnicode;
+                //RFID.RtsEnable = true;
+                //RFID.Handshake = Handshake.None; 
+                RFID.Open();
+                // RFID.ReadTimeout = 200;
+                // string[] ports = SerialPort.GetPortNames();
+                RFID.DataReceived += new SerialDataReceivedEventHandler(RFID_DataReceivedHandler);
+                // RFID.Close();
             }
         }
         public static string byteToHexStr(byte[] bytes)
